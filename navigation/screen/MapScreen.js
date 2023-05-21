5252c099125dda052d518cc22a52eecc5c5d1e19
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-05-21 16:40:27 -0500

    Made New MapScreen Page

---
+++ navigation/screen/MapScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MapScreen = () => {
+  return (
+    <View>
+      <Text>This is the map page</Text>
+    </View>
+  );
+};
+
+export default MapScreen;
