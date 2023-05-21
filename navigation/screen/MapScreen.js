53086d9035dccbe9937f87f7555e12bda830eafd
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-05-21 18:11:35 -0500

    Map Displaying User's Exact Location Added

--- navigation/screen/MapScreen.js
+++ navigation/screen/MapScreen.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 const MapScreen = () => {
-  return (
-    <View>
-      <Text>This is the map page</Text>
-    </View>
-  );
+    const [location, setLocation] = useState(null);
+
+    useEffect(() => {
+      (async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          console.error('Permission to access location was denied');
+          return;
+        }
+  
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+      })();
+    }, []);
+  
+    return (
+      <View style={{ flex: 1 }}>
+        {location && (
+          <MapView
+            style={{ flex: 1 }}
+            initialRegion={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          >
+            <Marker
+              coordinate={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+              }}
+              title="Your Location"
+            />
+          </MapView>
+        )}
+      </View>
+    );
 };
 
 export default MapScreen;
