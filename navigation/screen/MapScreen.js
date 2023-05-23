b3cdc4a832a6b4c68e1182a716c5d32cf0ea3298
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-05-23 10:08:05 -0500

    Notifications & Permission Requests Built

--- navigation/screen/MapScreen.js
+++ navigation/screen/MapScreen.js
@@ -1,14 +1,24 @@
-import { React, useState, useEffect } from 'react';
+import { React, useState, useEffect} from 'react';
 import { View, Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
 
 const MapScreen = () => {
     const [location, setLocation] = useState(null);
 
     useEffect(() => {
       (async () => {
-        let { status } = await Location.requestForegroundPermissionsAsync();
+        //let { status } = await Location.requestForegroundPermissionsAsync();
+        const { status } = await Notifications.requestPermissionsAsync();
         if (status !== 'granted') {
           console.error('Permission to access location was denied');
           return;
@@ -16,6 +26,16 @@ const MapScreen = () => {
   
         let location = await Location.getCurrentPositionAsync({});
         setLocation(location);
+
+        // Send a notification when the location is fetched
+      const notification = {
+        title: 'Map Screen',
+        body: 'Your location has been updated!',
+      };
+      await Notifications.scheduleNotificationAsync({
+        content: notification,
+        trigger: null,
+      });
       })();
     }, []);
   
